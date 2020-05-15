d8bd15d42bbe4983fb43ec01d64c4f76d9ce6756
Martin Slemr <mslemr@redhat.com>
2020-05-15 10:39:30 +0200

    Extend logger with common logging phrases

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.2] - 2020-05-15
+### Changed
+
+Extend logger with common logging phrases #22
+
 ## [1.0.1] - 2020-05-06
 ### Changed
 
@@ -15,5 +20,6 @@ manageiq-loggers to >= 0.4.2 #20
 ### Initial release to rubygems.org
 
 [Unreleased]: https://github.com/RedHatInsights/topological_inventory-providers-common/compare/v1.0.1...HEAD
+[1.0.2]: https://github.com/RedHatInsights/topological_inventory-providers-common/compare/v1.0.1...v1.0.2
 [1.0.1]: https://github.com/RedHatInsights/topological_inventory-providers-common/compare/v1.0.0...v1.0.1
 [1.0.0]: https://github.com/RedHatInsights/topological_inventory-providers-common/releases/v1.0.0
--- lib/topological_inventory/providers/common/logging.rb
+++ lib/topological_inventory/providers/common/logging.rb
@@ -3,13 +3,38 @@ require "manageiq/loggers"
 module TopologicalInventory
   module Providers
     module Common
+      module LoggingFunctions
+        def collecting(status, source, entity_type, refresh_state_uuid, total_parts = nil)
+          msg = "[#{status.to_s.upcase}] Collecting #{entity_type}"
+          msg += ", :total parts => #{total_parts}" if total_parts.present?
+          msg += ", :source_uid => #{source}, :refresh_state_uuid => #{refresh_state_uuid}"
+          info(msg)
+        end
+
+        def collecting_error(source, entity_type, refresh_state_uuid, exception)
+          msg = "[ERROR] Collecting #{entity_type}, :source_uid => #{source}, :refresh_state_uuid => #{refresh_state_uuid}"
+          msg += ":message => #{exception.message}\n#{exception.backtrace.join("\n")}"
+          error(msg)
+        end
+
+        def sweeping(status, source, sweep_scope, refresh_state_uuid)
+          msg = "[#{status.to_s.upcase}] Sweeping inactive records, :sweep_scope => #{sweep_scope}, :source_uid => #{source}, :refresh_state_uuid => #{refresh_state_uuid}"
+          info(msg)
+        end
+      end
+
+      class Logger < ManageIQ::Loggers::CloudWatch
+        def self.new(*args)
+          super.tap { |logger| logger.extend(TopologicalInventory::Providers::Common::LoggingFunctions) }
+        end
+      end
+
       class << self
         attr_writer :logger
       end
 
       def self.logger
-        @logger ||= ManageIQ::Loggers::Container.new
-        @logger
+        @logger ||= TopologicalInventory::Providers::Common::Logger.new
       end
 
       module Logging
